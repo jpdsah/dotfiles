073cff8c330b0a76ac3aa8dfe5d9182d48ca9896
Jageshwar Sah <jageshwar@krshnatech.com>
2023-12-17 17:54:01 +0530

    Added YouCompleteMe for vim

--- vim/install.sh
+++ vim/install.sh
@@ -13,6 +13,12 @@ vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 git clone https://tpope.io/vim/fugitive.git $VIM_PATH/pack/tpope/start/fugitive
 git clone https://github.com/vim-airline/vim-airline $VIM_PATH/pack/dist/start/vim-airline
 git clone https://github.com/udalov/kotlin-vim.git $VIM_PATH/pack/plugins/start/kotlin-vim
+git clone --recurse-submodules https://github.com/ycm-core/YouCompleteMe.git $VIM_PATH/pack/YouCompleteMe/opt/YouCompleteMe
+
+# Install YouCompleteMe
+pushd ~/.vim/pack/YouCompleteMe/opt/YouCompleteMe
+./install.py --all
+popd
 
 # Dracula Theme
 # mkdir -p ~/.vim/pack/themes/start
